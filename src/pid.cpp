8480bbba86e7b2128e91172e262d967330936880
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2018-09-27 15:35:47 +0200

    Testing floating point numbers

--- src/pid.cpp
+++ src/pid.cpp
@@ -16,15 +16,15 @@
 #include <stdlib.h>   // abs()
 #include "pid.h"
 
-PID::PID(const uint32_t _setpoint, const int32_t kp, const int32_t ki, const int32_t kd, uint8_t _qn, FeedbackDirection _feedbackDirection, ProportionalGain proportionalGain)
+PID::PID(const uint32_t _setpoint, const double kp, const double ki, const double kd, uint8_t _qn, FeedbackDirection _feedbackDirection, ProportionalGain proportionalGain)
     : feedbackDirection(_feedbackDirection), qn(_qn), setpoint(_setpoint) {
     this->setTunings(kp, ki, kd, proportionalGain);
 }
 
-PID::PID(const uint32_t setpoint, const int32_t kp, const int32_t ki, const int32_t kd, uint8_t qn, FeedbackDirection feedbackDirection)
+PID::PID(const double setpoint, const double kp, const double ki, const double kd, uint8_t qn, FeedbackDirection feedbackDirection)
     :PID::PID(setpoint, kp, ki, kd, qn, feedbackDirection, proportionalToError) {}
 
-PID::PID(const uint32_t setpoint, const int32_t kp, const int32_t ki, const int32_t kd, uint8_t qn)
+PID::PID(const uint32_t setpoint, const double kp, const double ki, const double kd, uint8_t qn)
     :PID::PID(setpoint, kp, ki, kd, qn, feedbackNegative) {}
 
 /**
@@ -43,15 +43,12 @@ const uint32_t PID::compute(const uint32_t input) {
     // Calcualte P term
     // Note: the calculation is (uint32_t)setpoint - (uint32_t)(input) = (int32_t)error (using signed math)
     // This is true for offset binary values!
-    const int32_t error = signed_subtract_saturated_32_and_32(this->setpoint, input);    // Subtract using saturating math
+    const double error = this->setpoint - input
     // Calcualte I term
     // TODO: Think about taking into account the previous result as well
     // -> Bilinear Transform instead of Backward difference
     // https://en.wikipedia.org/wiki/Bilinear_transform
-    errorSum = signed_multiply_accumulate_saturated_32_and_32QN(    // Calculate (errorSum + ki*error) using saturating math
-        this->errorSum,
-        this->ki,
-        error
+    errorSum+ = this->ki * error;
     );
     // Calculate D term (Note: We actually calcualte -dInput)
     // We do not calculate dError, because this would cause an output spike every time someone changes the setpoint
@@ -59,45 +56,32 @@ const uint32_t PID::compute(const uint32_t input) {
     //        = dSetpoint - dInput
     // We would like to get rid of the setpoint dependence and during normal operation, there is no difference
     //        ≈ -dInput
-    const int32_t dInputNegative = signed_subtract_saturated_32_and_32(this->previousInput, input);    // Subtract using saturating math
+    const double dInputNegative = this->previousInput - input;
 
     // Store the input to calculate the D-term next time
     this->previousInput = input;
 
     if (UNLIKELY(proportionalGain == proportionalToInput)) {
-        errorSum = signed_multiply_accumulate_saturated_32_and_32QN(
-            this->errorSum,
-            this->kp,
-            dInputNegative
-        );
+        errorSum+ = this->kp * dInputNegative;
     }
 
     // This will prevent integral windup
     this->errorSum = clamp(errorSum, outputMin, outputMax);
 
-    int32_t output = signed_multiply_accumulate_saturated_32_and_32QN(
-        this->errorSum,
-        this->kd,
-        dInputNegative
-    );
+    double output = errorSum + this->kd * dInputNegative;
 
     // Normal PID
     if (LIKELY(proportionalGain == proportionalToError)) {
-        output = signed_multiply_accumulate_saturated_32_and_32QN(
-            output,
-            this->kp,
-            error
-        );
+        output+ = this->kp * error;
     }
 
     output = clamp(output, outputMin, outputMax);
-    output ^= 0x80000000;   // Convert from 2s complement to offset Binary
-    return (uint32_t)output >> this->qn;
+    return (uint32_t)output;
 }
 
 /** Note: ki and kd must be normalized to the sampling time
  */
-void PID::setTunings(const int32_t kp, const int32_t ki, const int32_t kd, const ProportionalGain proportionalGain) {
+void PID::setTunings(const double kp, const double ki, const double kd, const ProportionalGain proportionalGain) {
     this->kp = (this->feedbackDirection == feedbackPositive) ? abs(kp) : -abs(kp);
     this->ki = (this->feedbackDirection == feedbackPositive) ? abs(ki) : -abs(ki);
     this->kd = (this->feedbackDirection == feedbackPositive) ? abs(kd) : -abs(kd);
@@ -105,16 +89,16 @@ void PID::setTunings(const int32_t kp, const int32_t ki, const int32_t kd, const
     this->proportionalGain = proportionalGain;
 }
 
-void PID::setTunings(const int32_t kp, const int32_t ki, const int32_t kd) {
+void PID::setTunings(const double kp, const double ki, const double kd) {
    this->setTunings(kp, ki, kd, this->proportionalGain);
 }
 
 void PID::setOutputMin(const uint32_t value) {
-    this->outputMin = (int32_t)((value << this->qn) ^ 0x80000000);    // Convert from Offset Binary to Integer
+    this->outputMin = value;    // Convert from Offset Binary to Integer
 }
 
 void PID::setOutputMax(const uint32_t value) {
-    this->outputMax = (int32_t)((value << this->qn) ^ 0x80000000);    // Convert from Offset Binary to Integer
+    this->outputMax = value;    // Convert from Offset Binary to Integer
 }
 
 void PID::setSetpoint(const uint32_t value) {
@@ -130,7 +114,7 @@ void PID::init(const uint32_t initialInput) {
 }
 
 void PID::updateOutput(const uint32_t value) {
-    this->errorSum = clamp((value << this->qn) ^ 0x80000000, this->outputMin, this->outputMax);
+    this->errorSum = clamp(value, this->outputMin, this->outputMax);
 }
 
 void PID::setControllerFeedback(const FeedbackDirection feedbackDirection) {
@@ -139,15 +123,15 @@ void PID::setControllerFeedback(const FeedbackDirection feedbackDirection) {
   this->setTunings(this->kp, this->ki, this->kd);
 }
 
-const uint32_t PID::getKp() {
+const double PID::getKp() {
     return this->kp;
 }
 
-const uint32_t PID::getKi() {
+const double PID::getKi() {
     return  this->ki;
 }
 
-const uint32_t PID::getKd() {
+const double PID::getKd() {
     return this->kd;
 }
 
